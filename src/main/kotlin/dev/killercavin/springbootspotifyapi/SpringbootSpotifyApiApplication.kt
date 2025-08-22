7ea808ec6ec727837f42d756234ac7de8fe71379
Cavin <cavinlarry001@gmail.com>
2025-08-22 04:29:37 +0300

    first commit

---
+++ src/main/kotlin/dev/killercavin/springbootspotifyapi/SpringbootSpotifyApiApplication.kt
@@ -0,0 +1,11 @@
+package dev.killercavin.springbootspotifyapi
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringbootSpotifyApiApplication
+
+fun main(args: Array<String>) {
+    runApplication<SpringbootSpotifyApiApplication>(*args)
+}
---
+++ src/test/kotlin/dev/killercavin/springbootspotifyapi/SpringbootSpotifyApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package dev.killercavin.springbootspotifyapi
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringbootSpotifyApiApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
